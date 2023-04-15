fdc979a8796ca53e71749571148404394a826f63
Luciano Garcia <43303397+lucianogarciagpi@users.noreply.github.com>
2023-04-15 14:26:00 -0300

    Update em adicionando a imagem em card1
    Descrevendo o que o código faz.

--- lib/card1.dart
+++ lib/card1.dart
@@ -18,23 +18,33 @@ class Card1 extends StatelessWidget {
     return Center(
       // TODO: Card1 Decorate Container
       child: Container(
-        //  1
+
+        //  1. Aplique um padding de 16 pixels em todos os lados do container
         padding: const EdgeInsets.all(16),
-        //  2
+
+        //  2. Restrinja o tamanho do contêiner a uma largura de 350
+        //  e uma altura de 450 pixels.
         constraints: const BoxConstraints.expand(
           width: 350,
           height: 450,
         ),
-        //  3
+
+        //  3. Aplicar um BoxDecoration, descreve como desenhar
+        //  uma caixa.
         decoration: const BoxDecoration(
-          //  4
+
+          //  4. Informa para pintar uma imagem
           image: DecorationImage(
-            //  5
+
+            //  5. define qual a imagem que é para ser pintada
             image: AssetImage('assets/mag1.png'),
-            //  6
+
+            //  6. Informa para cubrir a caixa inteira com essa imagem
             fit: BoxFit.cover,
           ),
-          //  7
+
+          //  7. Aplicando um raio de canto de 10 pixels em todos os lados
+          //  do recipiente.
           borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
         // TODO: Add a stack of text
