90552919bc852d9efddac6ff44a0e4ae5c05a92f
Luciano Garcia <43303397+lucianogarciagpi@users.noreply.github.com>
2023-04-15 16:03:30 -0300

    Incluindo o card2 na home.

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'card1.dart';
+import 'card2.dart';
 
-// 1
+//  1. Nova classe estende StatefulWidget
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -9,20 +11,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  //  7
+  //  TODO: Add state variables and functions
+  //  1. _selectedIndex controla qual guia está selecionada no momento.
+  //  O "_", significa que é privado. O indice selecionado é o estado que
+  //  esta sendo rastreado por _HomeState.
   int _selectedIndex = 0;
 
-  //  8 Widgets de cada guia
+  //  2. Definindo os Widgets que serão exibidos em cada guia, por enquanto
+  //  mostra cores.
   static List<Widget> pages = <Widget>[
-    //  TODO: Replace with Card1
-    Container(color: Colors.red),
-    //  TODO: Replace with Card2
-    Container(color: Colors.green),
+    const Card1(),
+    const Card2(),
     //  TODO: Replace with Card3
     Container(color: Colors.blue),
   ];
 
-  //  9
+  //  9.  Lida com itens da barra de tabulação tocados, aqui definimos o índice
+  //  do item que o usuário pressionou. setState() notifica a estrutura que o
+  //  estado desse objeto foi alterado, e em seguida reconstrói esse widget
+  //  internamente.
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -33,40 +40,46 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
-          'Fooderlich',
-          // 2
-          style:
-          Theme.of(context).textTheme.headline6,
+          'Foorderlich',
+          //  2. Theme.of(context) retorna o Theme mais próximo da árvore de widgets
+          //  Se o Widget tiver um tema definido, ele retorna esse, caso contrário
+          //  retorna o tema do aplicativo.
+          style: Theme.of(context).textTheme.headline6,
         ),
       ),
+      // TODO: Show selected tab
       body: pages[_selectedIndex],
-      // 4 Definindo barra inferior de navegação
+      //  TODO: Add bottom navigation bar
+      //  4. Definindo um BottomNavigationBar
       bottomNavigationBar: BottomNavigationBar(
-        //  5
-        selectedItemColor:
-        Theme.of(context).textSelectionTheme.selectionColor,
-        //  10
-        currentIndex: _selectedIndex,
-        //  11
-        onTap: _onItemTapped,
-        //  6
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.card_giftcard),
-              label: 'Card',
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.card_giftcard),
-              label: 'Card2',
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.card_giftcard),
-              label: 'Card3',
-          ),
+          //  5. Definindo a cor de seleção de um item quando tocado
+          selectedItemColor:
+              Theme.of(context).textSelectionTheme.selectionColor,
+          //  TODO: Set selected tab bar
+          //  10. Informa a barra de navegacao inferior qual item da barra de
+          //  guias destacar.
+          currentIndex: _selectedIndex,
+          //  11. O usuario ao tocar em um item na barra de guias, ´chamado o
+          //  manipulador de _omItemTapped, que atualiza o índice e muda a cor.
+          onTap: _onItemTapped,
 
-        ],
-      ),
+          //  6. Definindo três itens da barra de guias de navegação inferior
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard),
+              label: "Card",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard),
+              label: "Card2",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard),
+              label: "Card3",
+            ),
+          ]),
     );
   }
 }
