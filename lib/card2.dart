19617723e7275f1c09d8c7e2224744fb99da4742
Luciano Garcia <43303397+lucianogarciagpi@users.noreply.github.com>
2023-04-19 22:41:06 -0300

    Composing the text in card2.

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/main.dart';
+import 'author_card.dart';
+import 'fooderlich_theme.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
@@ -6,7 +9,6 @@ class Card2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-
       //  1. O Widget Center, tem um filho "Container" que tem três propriedades,
       //  sendo as duas primeiras "constraints", "decoration"
       child: Container(
@@ -24,9 +26,47 @@ class Card2 extends StatelessWidget {
         //  2. A terceira propriedade, é o column, que exibe seus widgets filhos
         //  na vertical.
         child: Column(
-          children: const [
+          children: [
             //  TODO 1: Add author information
+            const AuthorCard(
+              authorName: "Mike Katz",
+              title: "Smoothie Connoisseus",
+              imageProvider: AssetImage("assets/author_katz.jpeg"),
+            ),
             //  TODO 4: add Positioned text
+
+            //  1. Com esse widget, você preenche o restante do espaço
+            Expanded(
+
+              //  2. Aplique este widget para empilhar os widgets uns sobre os outros.
+              child: Stack(
+                children: [
+
+                  //  3. Posicione o primeiro widget, 16px inferior e 16 a direita
+                  Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: Text(
+                      'Recipe',
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                  ),
+
+                  //  4. Posicione o segundo widget a 70px da parte inferior e 16px da esquerda
+                  Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        'Smoothies',
+                        style: FooderlichTheme.lightTextTheme.headline1,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
